e0ecaf27daf4eae43d1c83e5dcc5998a19594fef
ducnt1003 <nguyentrungduc0310@gmail.com>
2022-07-17 10:27:42 +0700

    change admin logo

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{route('admin.orders.order')}}" class="brand-link">
-        <img src="/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="/template/dist/img/AdminLogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Rhust-app</span>
     </a>
 
